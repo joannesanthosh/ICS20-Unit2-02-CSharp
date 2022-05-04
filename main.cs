6945942d63d2d149ca5a51f867118c7d41002405
Joanne Santhosh <99355156+joannesanthosh@users.noreply.github.com>
2022-05-04 22:58:25 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,18 @@
+// Created by: Joanne Santhosh
+// Created on: Mar 2022
+//
+// This program does basic math
+
+using System; 
+
+public class Program 
+{
+
+    public static void Main(string[] args) 
+    {
+       Console.WriteLine  ("5 × 3 = " + (5 * 3));
+       Console.WriteLine("5 + 3 + 5 + 3 = " + (5 + 3 + 5 + 3));
+      
+       Console.WriteLine("\nDone.");
+    }
+}
