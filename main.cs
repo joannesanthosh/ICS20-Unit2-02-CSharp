79ab885e22d45a784ea773f3e4b0b71542ee7fde
Joanne Santhosh <99355156+joannesanthosh@users.noreply.github.com>
2022-06-05 17:20:39 +0000

    fix spacing

--- main.cs
+++ main.cs
@@ -5,10 +5,9 @@
 
 using System;
 
-public class Program
+class Program
 {
-
-    public static void Main (string[] args) 
+    public static void Main(string[] args) 
     {
         Console.WriteLine("The dimensions of a rectangle are 5 cm and 3 cm");
         Console.WriteLine("5 × 3 = " + (5 * 3) + "cm²");
